fd63d2134a4c7c3ad862de863390f55b609c44e0
gabpa <gabpa@DESKTOP-16H8VG8>
2021-10-02 22:56:15 -0300

    add test class for validation

---
+++ Test/EasyValidation.Core.Tests/ValidationTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace EasyValidation.Core.Tests;
+
+public class ValidationTests
+{
+    [Fact(DisplayName = "Should")]
+    public void Should()
+    {
+        //=>
+    }
+}
+
