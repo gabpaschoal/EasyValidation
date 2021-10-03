d1d3fe12bc7b42c96f26334a212a8b0cfe0af223
gabpa <gabpa@DESKTOP-16H8VG8>
2021-10-02 22:55:33 -0300

    test: should add a new valeu when key doesnt exists

---
+++ Test/EasyValidation.Core.Tests/Results/ResultDataTests.cs
@@ -0,0 +1,20 @@
+﻿using EasyValidation.Core.Results;
+using System.Linq;
+using Xunit;
+
+namespace EasyValidation.Core.Tests.Results;
+
+public class ResultDataTests
+{
+    [Fact(DisplayName = "Should add a new value when dont exist in the dictionary")]
+    public void Should_add_a_new_value_when_dont_exist_in_the_dictionary()
+    {
+        var resultData = new ResultData();
+
+        var key = "_key";
+
+        resultData.AddError(key, "This is a valid message!");
+
+        var data = resultData.Errors.Single(x => x.Key == key);
+    }
+}
