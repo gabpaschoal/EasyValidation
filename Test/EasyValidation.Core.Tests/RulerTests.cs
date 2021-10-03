03135411f25fa76bcbaaf2278f75631c655af4cf
gabpa <gabpa@DESKTOP-16H8VG8>
2021-10-03 08:11:02 -0300

    create MakeSut method

--- Test/EasyValidation.Core.Tests/RulerTests.cs
+++ Test/EasyValidation.Core.Tests/RulerTests.cs
@@ -1,9 +1,28 @@
-﻿using Xunit;
+﻿using EasyValidation.Core.Results;
+using EasyValidation.Core.Tests.Stubs;
+using System;
+using System.Linq.Expressions;
+using Xunit;
 
 namespace EasyValidation.Core.Tests;
 
 public class RulerTests
 {
+    private static Ruler<PeopleStubCommand, string> MakeSut(
+        IResultData resultData,
+        PeopleStubCommand peopleStub,
+        Expression<Func<PeopleStubCommand, string>> expression)
+    {
+        resultData ??= new ResultData();
+
+        var addressStub = new AddressStubCommand("St. 1", "Downtown", "NYC", 99);
+        peopleStub ??= new PeopleStubCommand("Maria", "Santos", 27, addressStub, addressStub);
+
+        Func<PeopleStubCommand, string> func = peopleStubCommand => peopleStubCommand.FirstName;
+        expression ??= peopleStubCommand => func(peopleStubCommand);
+
+        return new Ruler<PeopleStubCommand, string>(resultData, peopleStub, expression);
+    }
 
     [Fact(DisplayName = "Should")]
     public void Should()
