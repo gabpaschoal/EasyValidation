9eb1ff7a8eb597c84cb4df09e461192f4b971f80
gabpa <gabpa@DESKTOP-16H8VG8>
2021-10-02 22:55:12 -0300

    should add a new valeu when key doesnt exists

--- Src/EasyValidation.Core/Results/ResultData.cs
+++ Src/EasyValidation.Core/Results/ResultData.cs
@@ -1,5 +1,27 @@
-﻿namespace EasyValidation.Core.Results;
+﻿using System.Collections.ObjectModel;
+
+namespace EasyValidation.Core.Results;
 
 public class ResultData : IResultData
 {
+    public ResultData()
+    {
+        _errors = new Dictionary<string, IList<string>>();
+    }
+
+    private readonly IDictionary<string, IList<string>> _errors;
+
+    public IReadOnlyDictionary<string, IList<string>> Errors => new ReadOnlyDictionary<string, IList<string>>(_errors);
+
+    public void AddError(string key, string message)
+    {
+        if (!_errors.ContainsKey(key))
+        {
+            _errors.Add(key, new[] { message });
+            return;
+        }
+
+        var errors = _errors[key];
+        errors.Add(message);
+    }
 }
