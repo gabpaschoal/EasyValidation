d9575a1b6c6189005f70810c91cbfe397d9d44c7
gabpa <gabpa@DESKTOP-16H8VG8>
2021-10-11 22:38:52 -0300

    create a method to localize all validators by assembly

---
+++ EasyValidation.DependencyInjection/EasyValidationDI.cs
@@ -0,0 +1,31 @@
+﻿using EasyValidation.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace EasyValidation.DependencyInjection
+{
+    public static class EasyValidationDI
+    {
+        public static IServiceCollection AddEasyValidationValidators(this IServiceCollection service, params Assembly[] assemblies)
+        {
+            assemblies.SelectMany(x => x.GetTypes())
+                .Where(t => TypeFilter(t))
+                .Select(t => Activator.CreateInstance(t))
+                .ToList()
+                .ForEach(x =>
+                {
+                    if (x is null)
+                        return;
+
+                    service.AddTransient(x.GetType());
+                });
+
+            return service;
+        }
+
+        private static bool TypeFilter(Type t)
+        {
+            return t.GetInterfaces().Any(gi => gi.IsGenericType && gi.GetGenericTypeDefinition() == typeof(IValidation<>));
+        }
+    }
+}
