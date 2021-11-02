99d0590040b12e88bf5102fa52b11185c7d9e968
gabpa <gabpa@DESKTOP-16H8VG8>
2021-11-02 20:51:29 -0300

    add a typed exception to throw when don't find the validator

---
+++ Src/EasyValidation.DependencyInjection/Exceptions/ValidatorNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyValidation.DependencyInjection.Exceptions;
+
+public class ValidatorNotFoundException : Exception
+{
+    public ValidatorNotFoundException(Type commandType)
+        : base($"No validator found for command {commandType.Name}")
+    { }
+}
+
