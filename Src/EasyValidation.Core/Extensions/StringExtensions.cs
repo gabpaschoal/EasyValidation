2ab4112bd6f019b078d7e9e2a56399f718377746
gabpa <gabpa@DESKTOP-16H8VG8>
2021-11-02 21:26:35 -0300

    add string extensions

---
+++ Src/EasyValidation.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace EasyValidation.Core.Extensions;
+
+public static class StringExtensions
+{
+    public static IRuler<T, string> HasMinLenght<T>(this IRuler<T, string> ruler, int minLenght, string message = "Should have more than {0} digits")
+            => ruler.When(x => x.Length < minLenght).WithMessage(string.Format(message, minLenght));
+}
