76a79746112a7166d98d366bd303f1d12ca3130b
gabpa <gabpa@DESKTOP-16H8VG8>
2021-11-02 21:37:48 -0300

    add max lenght extension validation

--- Src/EasyValidation.Core/Extensions/StringExtensions.cs
+++ Src/EasyValidation.Core/Extensions/StringExtensions.cs
@@ -4,4 +4,6 @@ public static class StringExtensions
 {
     public static IRuler<T, string> HasMinLenght<T>(this IRuler<T, string> ruler, int minLenght, string message = "Should have more than {0} digits")
             => ruler.When(x => x.Length < minLenght).WithMessage(string.Format(message, minLenght));
+    public static IRuler<T, string> HasMaxLenght<T>(this IRuler<T, string> ruler, int maxLenght, string message = "Should have minus than {0} digits")
+            => ruler.When(x => x.Length > maxLenght).WithMessage(string.Format(message, maxLenght));
 }
