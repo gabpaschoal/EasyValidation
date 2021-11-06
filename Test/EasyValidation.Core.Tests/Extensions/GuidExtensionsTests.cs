4132de05c9f759bc3bc2086e5443ac1fc13f3f42
gabpa <gabpa@DESKTOP-16H8VG8>
2021-11-06 17:16:26 -0300

    feat add guid validations to check when is empty

---
+++ Test/EasyValidation.Core.Tests/Extensions/GuidExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using EasyValidation.Core.Extensions;
+using EasyValidation.Core.Results;
+using FluentAssertions;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace EasyValidation.Core.Tests.Extensions;
+
+public class GuidExtensionsTests
+{
+    private record GuidCommandStub(
+       Guid IsBeEmpty);
+
+    private class GuidValidatorStub : Validation<GuidCommandStub>
+    {
+        public override void Validate()
+        {
+            ForMember(x => x.IsBeEmpty).IsNotEmpty();
+        }
+    }
+
+    private static GuidCommandStub MakeValidModel()
+    {
+        return new(
+            IsBeEmpty: Guid.NewGuid()
+        );
+    }
+
+    private static IResultData MakeAndValidateSut(GuidCommandStub commandSut)
+    {
+        var stub = new GuidValidatorStub();
+        stub.SetValue(commandSut);
+        stub.Validate();
+
+        return stub.ResultData;
+    }
+
+    [Fact(DisplayName = "Should add an error when IsNotBeEmpty is Empty")]
+    public void Should_add_an_error_when_IsNotBeEmpty_is_Empty()
+    {
+        var model = MakeValidModel() with { IsBeEmpty = Guid.Empty };
+        var resultData = MakeAndValidateSut(model);
+
+        resultData.FieldErrors.Single().Key.Should().BeEquivalentTo(nameof(model.IsBeEmpty));
+    }
+}
